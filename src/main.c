855874c9355b08fe479b91e63f183af2348a1e01
dilaranarin <dilaraelifnarin@gmail.com>
2021-05-09 18:56:00 +0300

    mehehe

--- src/main.c
+++ src/main.c
@@ -17,8 +17,8 @@ int main(int argc, char* argv[]){
   argv[1] = "database.txt";
   char dna_seq[SIZE], gen_seq[SIZE];
   int i=0, k=0,respond=0;
-  int *ids = malloc(sizeof(int)*100);  
-  char *data = malloc(sizeof(char) * 100); 
+  int *ids = calloc(10,sizeof(int)*100);  
+  char *data = calloc(10,sizeof(char) * 100); 
   if(!data || !ids) exit(1);
 
   char fileloc[] = "../data/"; 
@@ -49,17 +49,14 @@ int main(int argc, char* argv[]){
       ids = realloc(ids, sizeof(int) * k);
       ids = find_similar_first(dna_seq, dbt, k);
       printf("Toplamda bulunan benzer birey sayisi: %d\n", k);
-       i=0;
+      i=0;
       while(1){
-       if(ids[i]==ids[i+1])
-         i++;
-        if(k<i+1) break;
-       find_binary(dbt,0,dbt->size-1,ids[i],dna_seq);
+      if(ids[i]==ids[i+1])
+      i++;
+      if(k<i+1) break;
+      find_binary(dbt,0,dbt->size-1,ids[i],dna_seq);
       i++;
       }
-      /*for(int i = 0, j; i < k; ++i){
-        find_binary(dbt, 0, dbt->size - 1, ids[i], dna_seq);
-      }*/
 	    break;
 
       case 3:
@@ -105,61 +102,3 @@ int main(int argc, char* argv[]){
   free(data);
   return 0;
 }
-
-
-
-/*int main(int argc,char* argv[]){
-  *  int i=0,x=0;
-   * char *data = malloc(sizeof(char) * 100);
-    *char *filename = malloc(sizeof(char) * 50);  
- *   if(!data || !filename) exit(1);
-  *  char fileloc[] = "../data/"; 
-  *  printf("Okunacak dosya ismini uzantisiz girin: ");
-*    scanf(" %[^\n]%*c", filename);
-*    while(strstr(filename, ".")){
-*        printf("Dosya ismi uzantisiz girilmelidir: ");
-*        scanf(" %[^\n]%*c", filename);
-*    }
-*    strcpy(data, fileloc);
-*    strcat(data, filename);
-*    strcat(data, ".txt");
-* 
-*    printf("Okunacak dosyanin adresi: %s\n", data);
-*    Dtbase *dbt = read_person_file(data);
-*    printf("Veri tabani okundu, hatali veri sayisi: %d\n",dbt->corrupted);
-*    
-*    char dna_seq[SIZE], gen_seq[SIZE/5];
-*   int k; 
-*    printf("DNA benzerligini bulmak icin bir DNA dizisi giriniz\n");
-*    scanf("%s", dna_seq);
-*    printf("En cok benzer kac tane veri istiyorsunuz?\n");
-*    scanf("%d", &k);
-*    int *ids = malloc(sizeof(int) * k);  
-*    ids = find_similar_first(dna_seq, dbt, k);
-*    printf("Toplamda bulunan benzer birey sayisi: %d\n", k);
-*    for(int i = 0, j; i < k; ++i){
-*        find_binary(dbt, 0, dbt->size - 1, ids[i], dna_seq);
-*    }
-*	
-*    // TODO: Kullanicidan bir gen dizisi isteyin.
-* .  printf("Geni bulmak icin bir gen dizisini giriniz: ");
-*        scanf("%s", gen_seq);
-*        int length=strlen(gen_seq);
-*    // // bu gen dizisine sahip bireyleri arayip, idlerini geri dondurun.
-*     ids = realloc(ids, sizeof(int) * 100);
-*         ids = find_gene_persons(gen_seq,length , *dbt);
-*    printf("If you want to see the ID list of people who have same gene, please press 1.Else, press 0.\n");
-*       scanf("%d",&x);
-*             if(x==1){
-*                     while(1){
-*                            if(ids[i]==ids[i+1])
-*                              break;
-*
-*                     printf("ID[ %d ] : %d \n",i+1,ids[i]);
-*                     i++;
-*                           }
-*                 }
-*
-*	return 0;
-* . } 
-*/
